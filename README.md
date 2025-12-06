413b59590a3e9d05815a38d79b992011000ff378
Giangblackk <giangblackk@gmail.com>
2025-12-06 22:43:28 +0700

    Add grpc dependency via Vcpkg (#1)
    * update grpc
    
    * set higher cpp std (17)
    
    * replace openssl by grpc quack
    
    * remove devcontainer folder (currently not working)
    
    * update readme

--- README.md
+++ README.md
@@ -1,15 +1,19 @@
 # DuckDB - Spark Connect Extension
 ## Introduction
 A project to build DuckDB extension to connect to Spark via Spark Connect API - so DuckDB can read/write data via Spark.
-## Setup Dev Container
-1. Update `vcpkg`
+
+## Setup VSCode Development Environment
+1. Install VSCode extensions:
+- clangd: `llvm-vs-code-extensions.vscode-clangd`
+- c/c++ extension pack: `ms-vscode.cpptools-extension-pack`
+
+2. Setup `clangd`:
+- after run `make` successfully, run script to tell clangd how your project is built (by create a link to built `compile_commands.json` file).
+
 ```bash
-/usr/local/vcpkg/bootstrap-vcpkg.sh
-```
-2. Install dependencies
-```bash
-vcpkg install
+ln -s build/release/compile_commands.json compile_commands.json
 ```
 
+3. Enjoy development.
 ## Documentation
 - [Extension Docs](./docs/README.md)
--- src/quack_extension.cpp
+++ src/quack_extension.cpp
@@ -1,12 +1,17 @@
+#include "duckdb/common/types.hpp"
+#include "duckdb/common/types/data_chunk.hpp"
+#include "duckdb/common/types/string_type.hpp"
+#include "duckdb/common/types/vector.hpp"
+#include "duckdb/common/vector_operations/unary_executor.hpp"
+#include "duckdb/execution/expression_executor_state.hpp"
 #define DUCKDB_EXTENSION_MAIN
 
 #include "quack_extension.hpp"
-#include "duckdb.hpp"
 #include "duckdb/common/exception.hpp"
 #include "duckdb/function/scalar_function.hpp"
 #include <duckdb/parser/parsed_data/create_scalar_function_info.hpp>
 
-// OpenSSL linked through vcpkg
+#include <grpc/grpc.h>
 #include <grpcpp/grpcpp.h>
 
 namespace duckdb {
@@ -18,23 +23,23 @@ inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &resu
 	});
 }
 
-// inline void QuackOpenSSLVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
-// 	auto &name_vector = args.data[0];
-// 	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
-// 		return StringVector::AddString(result, "Quack " + name.GetString() + ", my linked OpenSSL version is " +
-// 		                                           OPENSSL_VERSION_TEXT);
-// 	});
-// }
+inline void QuackgRPCVersionScalarFun(DataChunk &args, ExpressionState &state, Vector &result) {
+	auto &name_vector = args.data[0];
+	UnaryExecutor::Execute<string_t, string_t>(name_vector, result, args.size(), [&](string_t name) {
+		return StringVector::AddString(result, "Quack " + name.GetString() + ", my linked gRPC version is " +
+		                                           grpc_version_string());
+	});
+}
 
 static void LoadInternal(ExtensionLoader &loader) {
 	// Register a scalar function
 	auto quack_scalar_function = ScalarFunction("quack", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackScalarFun);
 	loader.RegisterFunction(quack_scalar_function);
 
-	// // Register another scalar function
-	// auto quack_openssl_version_scalar_function = ScalarFunction("quack_openssl_version", {LogicalType::VARCHAR},
-	//                                                             LogicalType::VARCHAR, QuackOpenSSLVersionScalarFun);
-	// loader.RegisterFunction(quack_openssl_version_scalar_function);
+	// register another scala function
+	auto quack_grpc_version_scalar_function =
+	    ScalarFunction("quack_grpc_version", {LogicalType::VARCHAR}, LogicalType::VARCHAR, QuackgRPCVersionScalarFun);
+	loader.RegisterFunction(quack_grpc_version_scalar_function);
 }
 
 void QuackExtension::Load(ExtensionLoader &loader) {
